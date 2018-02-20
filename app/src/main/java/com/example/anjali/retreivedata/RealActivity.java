d6c89a49cdc9ca2b5f538e7474033941ad14eea2
anjalicprs <anjalicprs@gmail.com>
2018-02-21 01:47:58 +0530

    last one

--- app/src/main/java/com/example/anjali/retreivedata/RealActivity.java
+++ app/src/main/java/com/example/anjali/retreivedata/RealActivity.java
@@ -1,23 +1,37 @@
 package com.example.anjali.retreivedata;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -38,22 +52,29 @@ public class RealActivity extends AppCompatActivity {
 
     private ProgressDialog pDialog;
     private ListView lv;
+    private String[] xData = new String[2];
+    private float[] yData = new float[2];
 
     // URL to get contacts JSON
     private static String url = null;
     LineChart lineChart;
     PieChart piechart;
+    String result = null;
     public static ArrayList<HashMap<String, String>> contactList;
-
+    float apl1 = 0;
+    float apl2=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Intent i = getIntent();
         Bundle bundle = i.getExtras();
         url = (String) bundle.get("url");
         Log.e(TAG, "The received URL is :: " + url);
-        new GetContacts().execute();
+        if(internetConCheck()== true){
+            new GetContacts().execute();
+        }
     }
 
     /**
@@ -77,14 +98,7 @@ public class RealActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
-            HttpHandler sh = new HttpHandler();
-
-
             String inputLine;
-            String jsonStr = sh.makeServiceCall(url);
-            Log.e(TAG, "Jason string is :: " + jsonStr);
-            String result = null;
-            //Create a connection
             URL myUrl = null;
             try {
                 myUrl = new URL(url);
@@ -133,6 +147,10 @@ public class RealActivity extends AppCompatActivity {
              * */
 
             pieChart(result);
+            TextView aplval = (TextView) findViewById(R.id.Id1val);
+            TextView ap2val = (TextView) findViewById(R.id.Id2val);
+            aplval.setText("Total units consumed by appliance 1 : "+Float.toString(yData[0]));
+            ap2val.setText("Total units consumed by appliance 2 : "+Float.toString(yData[1]));
         }
 
     }
@@ -140,7 +158,7 @@ public class RealActivity extends AppCompatActivity {
     {
        //piechart = (PieChart)findViewById(R.id.idpiechart);
         lineChart = (LineChart) findViewById(R.id.lineChart);
-        float[][] appliance = new float[2][5];
+        float[][] appliance = new float[2][32];
         String[] words=result.split(":");
         System.out.println(words[0]);
         System.out.println(words[1]);
@@ -148,14 +166,18 @@ public class RealActivity extends AppCompatActivity {
         System.out.println(words[3]);
         String[] applia1 = words[1].split(",");
         String[] applia2 = words[2].split(",");
+        yData[0]=0;
+        yData[1]=1;
         for(int i=1;i<applia1.length-1;i++)
         {
             appliance[0][i-1]=Float.valueOf(applia1[i]);
+            yData[0]+=appliance[0][i-1];
             System.out.println(appliance[0][i-1]);
         }
         for(int i=1;i<applia2.length-1;i++)
         {
-            appliance[1][i-1]=Float.valueOf(applia1[i]);
+            appliance[1][i-1]=Float.valueOf(applia2[i]);
+            yData[1]+=appliance[1][i-1];
         }
         ArrayList<String> xAXES = new ArrayList<>();
         ArrayList<Entry> yAXISsin = new ArrayList<>();
@@ -167,6 +189,8 @@ public class RealActivity extends AppCompatActivity {
             yAXIScos.add(new Entry(i,appliance[1][i]));
             xAXES.add(i, String.valueOf(i));
         }
+        System.out.println(yAXIScos);
+        System.out.println(yAXISsin);
         String[] xaxes = new String[xAXES.size()];
         for(int i=0; i<xAXES.size();i++){
             xaxes[i] = xAXES.get(i).toString();
@@ -185,6 +209,134 @@ public class RealActivity extends AppCompatActivity {
         lineDataSets.add(lineDataSet2);
 
         lineChart.setData(new LineData(lineDataSets));
-        lineChart.setVisibleXRangeMaximum(65f);
+        lineChart.setVisibleXRangeMaximum(20f);
+        lineChart.invalidate();
+        lineChart.refreshDrawableState();
+        xData[0]="AC";
+        xData[1]="fridge";
+        piechart = (PieChart) findViewById(R.id.pieChart);
+        piechart.setRotationEnabled(true);
+        piechart.setUsePercentValues(true);
+        //pieChart.setHoleColor(Color.BLUE);
+        //pieChart.setCenterTextColor(Color.BLACK);
+        piechart.setHoleRadius(25f);
+        piechart.setTransparentCircleAlpha(0);
+        piechart.setCenterText("Sangam !");
+        piechart.setCenterTextSize(10);
+        //pieChart.setDrawEntryLabels(true);
+        //pieChart.setEntryLabelTextSize(20);
+        //More options just check out the documentation!
+
+        addDataSet();
+        piechart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                Log.d(TAG, "onValueSelected: Value select from chart.");
+                Log.d(TAG, "onValueSelected: " + e.toString());
+                Log.d(TAG, "onValueSelected: " + h.toString());
+                //Toast.makeText(MainActivity.this, "onValueSelected: " + e.toString(),Toast.LENGTH_LONG).show();
+                int pos1 = e.toString().indexOf("y:");
+                String sales = e.toString().substring(pos1 + 2);
+
+                for(int i = 0; i < yData.length; i++){
+                    if(yData[i] == Float.parseFloat(sales)){
+                        pos1 = i;
+                        break;
+                    }
+                }
+                String employee = xData[pos1];
+                Toast.makeText(RealActivity.this, "Appliance " + employee + "\n" + "Units consumed :: " + sales + " KWh", Toast.LENGTH_SHORT).show();
+
+
+
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
     }
+    private void addDataSet() {
+        Log.d(TAG, "addDataSet started");
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
+
+        for(int i = 0; i < yData.length; i++){
+            yEntrys.add(new PieEntry(yData[i] , i));
+        }
+
+        for(int i = 1; i < xData.length; i++){
+            xEntrys.add(xData[i]);
+        }
+
+        //create the data set
+        PieDataSet pieDataSet = new PieDataSet(yEntrys, "Contribution of each appliance");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        //add colors to dataset
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.GRAY);
+        colors.add(Color.BLUE);
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+        colors.add(Color.CYAN);
+        colors.add(Color.YELLOW);
+        colors.add(Color.MAGENTA);
+
+        pieDataSet.setColors(colors);
+
+        //add legend to chart
+        Legend legend = piechart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setEnabled(true);
+
+        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+
+        //create pie data object
+        PieData pieData = new PieData(pieDataSet);
+        piechart.setData(pieData);
+        piechart.invalidate();
+    }
+    private boolean internetConCheck() {
+        if (internet_connection()){
+            Log.d(TAG, "cool");
+            return true;
+        }else{
+            //create a snackbar telling the user there is no internet connection and issuing a chance to reconnect
+            AlertDialog.Builder alertDialogBuilder= new AlertDialog.Builder(RealActivity.this);
+            alertDialogBuilder
+                    .setMessage("No internet connection")
+                    .setCancelable(false)
+                    .setPositiveButton("Try again ",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                }
+                            })
+                    .setNegativeButton("EXIT",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    finish();
+                                }
+                            });
+            AlertDialog alertDialog=alertDialogBuilder.create();
+            alertDialog.show();
+        }
+        return false;
+    }
+    boolean internet_connection(){
+        //Check if connected to internet, output accordingly
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
+    }
+
 }
--- app/src/main/java/com/example/anjali/retreivedata/SelectView.java
+++ app/src/main/java/com/example/anjali/retreivedata/SelectView.java
@@ -13,88 +13,62 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
-    String url = "http://165.227.105.231/sangam/authenticate.php";
+public class SelectView extends AppCompatActivity {
+    String url = "";
     private ProgressDialog pDialog;
     String result;
-    String passWord, userName;
+    float totalSum=0;
     private String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        internetConCheck();
-    }
-    public void register(View v){
-        EditText user = (EditText) findViewById(R.id.username);
-        EditText pass = (EditText) findViewById(R.id.pass);
-        userName = user.getText().toString();
-        passWord = pass.getText().toString();
-        url = url+"?mod=1&user="+userName+"&pass="+passWord;
-        boolean isConnected = internetConCheck();
-        if(isConnected == true){
-            new MainActivity.GetContacts().execute();
-        }
-    }
-
-    private boolean internetConCheck() {
-        if (internet_connection()){
-            Log.d(TAG, "cool");
-            return true;
-        }else{
-            //create a snackbar telling the user there is no internet connection and issuing a chance to reconnect
-            AlertDialog.Builder alertDialogBuilder= new AlertDialog.Builder(MainActivity.this);
-            alertDialogBuilder
-                    .setMessage("No internet connection")
-                    .setCancelable(false)
-                    .setPositiveButton("Try again ",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                                }
-                            })
-                    .setNegativeButton("EXIT",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    finish();
-                                }
-                            });
-            AlertDialog alertDialog=alertDialogBuilder.create();
-            alertDialog.show();
-        }
-        return false;
-    }
-
-    public void login(View v){
-        EditText user = (EditText) findViewById(R.id.username);
-        EditText pass = (EditText) findViewById(R.id.pass);
-        userName = user.getText().toString();
-        passWord = pass.getText().toString();
-        url = url+"?mod=2&user="+userName+"&pass="+passWord;
+        setContentView(R.layout.activity_select_view);
         if(internetConCheck()== true){
-            new MainActivity.GetContacts().execute();
-            Log.d(TAG, url);
+            url="http://165.227.105.231/sangam/month.php";
+            new SelectView.GetContacts().execute();
         }
     }
-
-    private void startMain() {
-        Log.d(TAG, "hey");
-        Intent i = new Intent(this, SelectView.class);
-        i.putExtra("name",userName);
+    public void doneSelection(View v){
+        Button hour = (Button)findViewById(R.id.month);
+        Button day = (Button) findViewById(R.id.day);
+        Button week = (Button) findViewById(R.id.week);
+        switch(v.getId()) {
+            case R.id.month:
+                    url = "http://165.227.105.231/sangam/month.php";
+                break;
+            case R.id.day:
+                url = "http://165.227.105.231/sangam/week.php";
+                break;
+            case R.id.week:
+                url = "http://165.227.105.231/sangam/day.php";
+                break;
+        }
+        show();
+    }
+    public void show(){
+        Intent i = new Intent(this, RealActivity.class);
+        i.putExtra("url", url);
         startActivity(i);
     }
-
     class GetContacts extends AsyncTask<String, Void, String> {
         final String REQUEST_METHOD = "GET";
         final int READ_TIMEOUT = 15000;
@@ -104,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onPreExecute() {
             super.onPreExecute();
             // Showing progress dialog
-            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog = new ProgressDialog(SelectView.this);
             pDialog.setMessage("Please wait...");
             pDialog.setCancelable(false);
             pDialog.show();
@@ -157,21 +131,67 @@ public class MainActivity extends AppCompatActivity {
             // Dismiss the progress dialog
             if (pDialog.isShowing())
                 pDialog.dismiss();
-            if(result.equals("authenticated"))
+            String[] words=result.split(":");
+            System.out.println(words[0]);
+            System.out.println(words[1]);
+            System.out.println(words[2]);
+            System.out.println(words[3]);
+            String[] applia1 = words[1].split(",");
+            String[] applia2 = words[2].split(",");
+            for(int i=1;i<applia1.length-1;i++)
             {
-                startMain();
+                totalSum+=Float.valueOf(applia1[i]);
             }
-            else if(result.equals("registered")){
-                startMain();
+            for(int i=1;i<applia2.length-1;i++)
+            {
+                totalSum+=Float.valueOf(applia2[i]);
+            }
+            float totalMoney;
+            TextView totalval = (TextView) findViewById(R.id.units);
+            totalval.setText("Total units consumed : "+Float.toString(totalSum));
+            TextView totalBal = (TextView) findViewById(R.id.money);
+            if(totalSum > 400)
+            {
+                totalMoney = totalSum * 3;
+            }
+            else if(totalSum > 200){
+                totalMoney = totalSum*2;
             }
             else{
-
+                totalMoney= totalSum;
             }
-            /**
-             * Updating parsed JSON data into ListView
-             * */
+            totalBal.setText("Total Money to be paid: "+Float.toString(totalMoney));
         }
 
+    }
+    private boolean internetConCheck() {
+        if (internet_connection()){
+            Log.d(TAG, "cool");
+            return true;
+        }else{
+            //create a snackbar telling the user there is no internet connection and issuing a chance to reconnect
+            AlertDialog.Builder alertDialogBuilder= new AlertDialog.Builder(SelectView.this);
+            alertDialogBuilder
+                    .setMessage("No internet connection")
+                    .setCancelable(false)
+                    .setPositiveButton("Try again ",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                }
+                            })
+                    .setNegativeButton("EXIT",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    finish();
+                                }
+                            });
+            AlertDialog alertDialog=alertDialogBuilder.create();
+            alertDialog.show();
+        }
+        return false;
     }
     boolean internet_connection(){
         //Check if connected to internet, output accordingly
@@ -182,5 +202,8 @@ public class MainActivity extends AppCompatActivity {
         boolean isConnected = activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
         return isConnected;
+    }
+    public void about(View v){
+
     }
 }
