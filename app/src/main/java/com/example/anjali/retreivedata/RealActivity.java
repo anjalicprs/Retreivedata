724e8a24ebda52e93aca46b5f2b476924525f10f
SuryaChaitanya <mb.surya96@gmail.com>
2018-02-20 19:53:10 +0530

    UI scroll activity

--- app/src/main/java/com/example/anjali/retreivedata/RealActivity.java
+++ app/src/main/java/com/example/anjali/retreivedata/RealActivity.java
@@ -1,15 +1,29 @@
 package com.example.anjali.retreivedata;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,10 +44,14 @@ public class RealActivity extends AppCompatActivity {
 
     private ProgressDialog pDialog;
     private ListView lv;
+    private String[] xData = new String[2];
+    private float[] yData = new float[2];
 
     // URL to get contacts JSON
     private static String url = null;
-    PieChart pieChart;
+    LineChart lineChart;
+    PieChart piechart;
+    String result = null;
     public static ArrayList<HashMap<String, String>> contactList;
 
     @Override
@@ -68,14 +86,13 @@ public class RealActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
-            HttpHandler sh = new HttpHandler();
+            //HttpHandler sh = new HttpHandler();
 
 
             String inputLine;
-            String jsonStr = sh.makeServiceCall(url);
-            Log.e(TAG, "Jason string is :: " + jsonStr);
-            String result = null;
-            //Create a connection
+//            String jsonStr = sh.makeServiceCall(url);
+//            Log.e(TAG, "Jason string is :: " + jsonStr);
+//            //Create a connection
             URL myUrl = null;
             try {
                 myUrl = new URL(url);
@@ -129,28 +146,151 @@ public class RealActivity extends AppCompatActivity {
     }
     public void pieChart(String result)
     {
-        try
+        //piechart = (PieChart)findViewById(R.id.idpiechart);
+        lineChart = (LineChart) findViewById(R.id.lineChart);
+        float[][] appliance = new float[2][32];
+        String[] words=result.split(":");
+        System.out.println(words[0]);
+        System.out.println(words[1]);
+        System.out.println(words[2]);
+        System.out.println(words[3]);
+        String[] applia1 = words[1].split(",");
+        String[] applia2 = words[2].split(",");
+        yData[0]=0;
+        yData[1]=1;
+        for(int i=1;i<applia1.length-1;i++)
         {
-            float[][] array = new float[2][5];
-            String[] words=result.split(":");
-            System.out.println(words[0]);
-            System.out.println(words[1]);
-            System.out.println(words[2]);
-            System.out.println(words[3]);
-            String[] applia1 = words[1].split(",");
-            String[] applia2 = words[2].split(",");
-            for(int i=1;i<applia1.length-1;i++)
-            {
-                array[0][i-1]=Float.valueOf(applia1[i]);
-                System.out.println(array[0][i-1]);
-            }
-            for(int i=1;i<applia2.length-1;i++)
-            {
-                array[1][i-1]=Float.valueOf(applia1[i]);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-
+            appliance[0][i-1]=Float.valueOf(applia1[i]);
+            yData[0]+=appliance[0][i-1];
+            System.out.println(appliance[0][i-1]);
         }
+        for(int i=1;i<applia2.length-1;i++)
+        {
+            appliance[1][i-1]=Float.valueOf(applia2[i]);
+            yData[1]+=appliance[1][i-1];
+        }
+        ArrayList<String> xAXES = new ArrayList<>();
+        ArrayList<Entry> yAXISsin = new ArrayList<>();
+        ArrayList<Entry> yAXIScos = new ArrayList<>();
+        double x = 0 ;
+        int numDataPoints = applia2.length-2;
+        Log.e(TAG, "ALALALA ALALLALA LA ALA"+numDataPoints);
+        //change
+        for(int i=0;i<numDataPoints;i++){
+            yAXISsin.add(new Entry(i,appliance[0][i]));
+            yAXIScos.add(new Entry(i,appliance[1][i]));
+            xAXES.add(i, String.valueOf(i));
+        }
+        System.out.println(yAXIScos);
+        System.out.println(yAXISsin);
+        String[] xaxes = new String[xAXES.size()];
+        for(int i=0; i<xAXES.size();i++){
+            xaxes[i] = xAXES.get(i).toString();
+        }
+
+        ArrayList<ILineDataSet> lineDataSets = new ArrayList<>();
+        LineDataSet lineDataSet1 = new LineDataSet(yAXIScos,"fan");
+        LineDataSet lineDataSet2 = new LineDataSet(yAXISsin,"fridge");
+
+        lineDataSet1.setDrawCircles(true);
+        lineDataSet1.setColors(Color.RED);
+        lineDataSet2.setDrawCircles(true);
+        lineDataSet2.setColors(Color.BLUE);
+
+        lineDataSets.add(lineDataSet1);
+        lineDataSets.add(lineDataSet2);
+
+        lineChart.setData(new LineData(lineDataSets));
+        lineChart.setVisibleXRangeMaximum(20f);
+        lineChart.invalidate();
+        lineChart.refreshDrawableState();
+        xData[0]="AC";
+        xData[1]="fridge";
+        piechart = (PieChart) findViewById(R.id.pieChart);
+        piechart.setRotationEnabled(true);
+        piechart.setUsePercentValues(true);
+        //pieChart.setHoleColor(Color.BLUE);
+        //pieChart.setCenterTextColor(Color.BLACK);
+        piechart.setHoleRadius(25f);
+        piechart.setTransparentCircleAlpha(0);
+        piechart.setCenterText("Sangam !");
+        piechart.setCenterTextSize(10);
+        //pieChart.setDrawEntryLabels(true);
+        //pieChart.setEntryLabelTextSize(20);
+        //More options just check out the documentation!
+
+        addDataSet();
+        piechart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                Log.d(TAG, "onValueSelected: Value select from chart.");
+                Log.d(TAG, "onValueSelected: " + e.toString());
+                Log.d(TAG, "onValueSelected: " + h.toString());
+                //Toast.makeText(MainActivity.this, "onValueSelected: " + e.toString(),Toast.LENGTH_LONG).show();
+                int pos1 = e.toString().indexOf("y:");
+                String sales = e.toString().substring(pos1 + 2);
+
+                for(int i = 0; i < yData.length; i++){
+                    if(yData[i] == Float.parseFloat(sales)){
+                        pos1 = i;
+                        break;
+                    }
+                }
+                String employee = xData[pos1];
+                Toast.makeText(RealActivity.this, "Appliance " + employee + "\n" + "Units consumed :: " + sales + " KWh", Toast.LENGTH_SHORT).show();
+
+
+
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
     }
+    private void addDataSet() {
+        Log.d(TAG, "addDataSet started");
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
+
+        for(int i = 0; i < yData.length; i++){
+            yEntrys.add(new PieEntry(yData[i] , i));
+        }
+
+        for(int i = 1; i < xData.length; i++){
+            xEntrys.add(xData[i]);
+        }
+
+        //create the data set
+        PieDataSet pieDataSet = new PieDataSet(yEntrys, "Contribution of each appliance");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        //add colors to dataset
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.GRAY);
+        colors.add(Color.BLUE);
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+        colors.add(Color.CYAN);
+        colors.add(Color.YELLOW);
+        colors.add(Color.MAGENTA);
+
+        pieDataSet.setColors(colors);
+
+        //add legend to chart
+        Legend legend = piechart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setEnabled(true);
+
+        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+
+        //create pie data object
+        PieData pieData = new PieData(pieDataSet);
+        piechart.setData(pieData);
+        piechart.invalidate();
+    }
+
+
 }
